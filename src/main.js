48fcfe849a6c0fa0b9e6a23ae7469429d6dd7944
hieu-ng <hieutnguyen06@gmail.com>
2020-06-06 16:41:20 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+require('../node_modules/bootstrap/dist/css/bootstrap.css')
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
